6eb560afcc001a88f927f72410dc2791f71d7571
Adam Góra <adamgora@icloud.com>
2021-09-20 07:52:41 +0200

    Added default value for database field of wallet balance

--- migrations/Version20210920054727.php
+++ migrations/Version20210920054727.php
@@ -21,7 +21,7 @@ final class Version20210920054727 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE account (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE wallet (id INT AUTO_INCREMENT NOT NULL, account_id INT NOT NULL, balance INT NOT NULL, INDEX IDX_7C68921F9B6B5FBA (account_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE wallet (id INT AUTO_INCREMENT NOT NULL, account_id INT NOT NULL, balance INT NOT NULL DEFAULT 0, INDEX IDX_7C68921F9B6B5FBA (account_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE wallet_operation (id INT AUTO_INCREMENT NOT NULL, wallet_id INT NOT NULL, type VARCHAR(20) NOT NULL, details LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', INDEX IDX_4E1DB6FC712520F3 (wallet_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE wallet ADD CONSTRAINT FK_7C68921F9B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id)');
         $this->addSql('ALTER TABLE wallet_operation ADD CONSTRAINT FK_4E1DB6FC712520F3 FOREIGN KEY (wallet_id) REFERENCES wallet (id)');
